b5a14d0a7a800dabcf5d1084315f19888d7c6dff
kohla <kohl.alu@gmail.com>
2024-05-15 22:52:49 +0200

    First commit

---
+++ src/main/java/com/infolk/debattierendatenbankprojekt/DebattierenDatenbankProjektApplication.java
@@ -0,0 +1,13 @@
+package com.infolk.debattierendatenbankprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebattierenDatenbankProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebattierenDatenbankProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infolk/debattierendatenbankprojekt/DebattierenDatenbankProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infolk.debattierendatenbankprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebattierenDatenbankProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
